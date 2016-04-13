9de70c1217a832a804442ac2e75b4dcae6d02252
Cassia Almeida <lisa.shiphrah@gmail.com>
2016-04-13 17:06:25 -0300

    Update Car.cs

--- Backup/VouJuntoCom/DAO/Car.cs
+++ Backup/VouJuntoCom/DAO/Car.cs
@@ -11,11 +11,11 @@ namespace VouJuntoCom.DAO
 	public class Car
 	{
 		public Guid ID { get; set; }
-		public string Make { get; set; }
+		public string Make { get; set; } 
 		public string Model { get; set; }
-		public string Color { get; set; }
+		public string Color { get; set; } 
 		public string RENAVAM { get; set; }
-		public int? Digits { get; set; }
+		public int? Digits { get; set; } 
 		public bool ArConditioning { get; set; }
 		public bool Radio { get; set; }
 		public bool Smoke { get; set; }
